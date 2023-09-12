0ad2e6aaa1d3fd65c39d9eb1b3e04ce67e42df65
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-09-12 18:18:31 +0200

    isMockSettingsONLocal

--- android/src/main/java/com/mock/location/LocationPlugin.java
+++ android/src/main/java/com/mock/location/LocationPlugin.java
@@ -5,6 +5,8 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.JSObject;
+import android.provider.Settings;
+import android.content.Context;
 
 
 @CapacitorPlugin(name = "Location")
@@ -20,18 +22,17 @@ public class LocationPlugin extends Plugin {
     @PluginMethod()
     public void isMockSettingsON(PluginCall call) {
         JSObject ret = new JSObject();
-        ret.put("value", isMockSettingsONLocal());
+        ret.put("value", isMockSettingsONLocal(getContext()));
         call.resolve(ret);
     }
 
-    private boolean isMockSettingsONLocal() {
+    private boolean isMockSettingsONLocal(Context context) {
         //Context context
         // returns true if mock location enabled, false if not enabled.
-        /* if (Settings.Secure.getString(context.getContentResolver(),
+        if (Settings.Secure.getString(context.getContentResolver(),
                 Settings.Secure.ALLOW_MOCK_LOCATION).equals("0"))
             return false;
         else
-            return true;*/
-        return false;
+            return true;
     }
 }
