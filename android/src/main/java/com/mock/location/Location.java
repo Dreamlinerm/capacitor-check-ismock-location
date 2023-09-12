08e767bc1dec6a663e56cda52465b58f39a58081
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-09-12 17:45:17 +0200

    fixed errors

--- android/src/main/java/com/mock/location/Location.java
+++ android/src/main/java/com/mock/location/Location.java
@@ -1,16 +1,16 @@
 package com.mock.location;
 
 import android.util.Log;
-
+import com.getcapacitor.PluginCall;
 public class Location {
 
-    public void isMock() {
-        Log.i("isMock");
+    public String isMock() {
+        // Log.i("isMock");
         return "isMock";
     }
 
-    public boolean isMockSettingsON(PluginCall call) {
-        Log.i("isMockSettingsON");
+    public String isMockSettingsON(PluginCall call) {
+        // Log.i("isMockSettingsON");
         return "isMock";
     }
 }
