c7401687cb7f096a9abdb8b642c7b0c8ba5035b0
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-01-27 21:39:53 +0100

    I always get could not build issues

--- ios/Plugin/LocationPlugin.swift
+++ ios/Plugin/LocationPlugin.swift
@@ -7,17 +7,17 @@ import Capacitor
  */
 @objc(LocationPlugin)
 public class LocationPlugin: CAPPlugin {
-    private let implementation = Location()
+    // private let implementation = Location()
 
-    @objc override func isMocked(_ call: CAPPluginCall) {
-        return false
-        // call.unimplemented("Not yet implemented on iOS.")
-    }
-    @objc override func isLastLocationMocked(_ call: CAPPluginCall) {
-        return false
-        // call.unimplemented("Not yet implemented on iOS.")
-    }
-    @objc override func installedMockPermissionApps(_ call: CAPPluginCall) {
-        call.unimplemented("Not implemented on iOS.")
-    }
+    // @objc override func isMocked(_ call: CAPPluginCall) {
+    //     return false
+    //     // call.unimplemented("Not yet implemented on iOS.")
+    // }
+    // @objc override func isLastLocationMocked(_ call: CAPPluginCall) {
+    //     return false
+    //     // call.unimplemented("Not yet implemented on iOS.")
+    // }
+    // @objc override func installedMockPermissionApps(_ call: CAPPluginCall) {
+    //     call.unimplemented("Not implemented on iOS.")
+    // }
 }
